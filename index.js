a0880ee005ef597ceb1f7f8f03ada5ab8864f8eb
unknown <vid2dhan@gmail.com>
2020-06-01 15:29:17 +0200

    comments resolved

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
- let date = new Date();
- let n = date.toDateString();
- let time = date.toLocaleTimeString();
+ const date = new Date();
+ const n = date.toDateString();
+ const time = date.toLocaleTimeString();
  document.getElementById('timeandDate').innerHTML = n + ' ,' + time;
  document.getElementById('timeandDate').style.color = "white";
  document.getElementById('timeandDate').style.marginTop = "200px";
  document.getElementById('timeandDate').style.marginLeft = "250px";
  document.getElementById('timeandDate').style.fontSize = "25px";
 
- let temperatureCmp = document.getElementById("weatherTemperature");
- let nameCmp = document.getElementById("name");
- let weatherMaxTempCmp = document.getElementById("weatherMaxTemp");
- let weatherCloudyCmp = document.getElementById("weatherCloudy");
- let weatherWindCmp = document.getElementById("weatherWind");
- let weatherSearchBar = document.getElementById("searchBar");
- let searchBtnCmp = document.getElementById("searchBtn");
- let weatherIconCmp = document.getElementById("weatherIcon");
+ const temperatureCmp = document.getElementById("weatherTemperature");
+ const nameCmp = document.getElementById("name");
+ const weatherMaxTempCmp = document.getElementById("weatherMaxTemp");
+ const weatherCloudyCmp = document.getElementById("weatherCloudy");
+ const weatherWindCmp = document.getElementById("weatherWind");
+ const weatherSearchBar = document.getElementById("searchBar");
+ const searchBtnCmp = document.getElementById("searchBtn");
+ const weatherIconCmp = document.getElementById("weatherIcon");
 
  searchBtnCmp.addEventListener("click", function() {
      fetch('https://api.openweathermap.org/data/2.5/weather?q=' + weatherSearchBar.value + '&units=metric&appid=08a09c29086a3f06cd37337b12b1711f')
          .then(response => response.json())
          .then(data => {
-             let currentTemperature = data.main.temp;
-             let maxTempValue = data.main.temp_max;
-             let windyValue = data.wind.speed;
-             let cloudValue = data.weather[0].description;
-             let placeNameValue = data.name;
              let iconNew = data.weather[0].icon;
              let imageUrl = "http://openweathermap.org/img/wn/" + iconNew + "@2x.png";
              weatherIconCmp.innerHTML = "<img src=" + imageUrl + ">";
              let newPlaceName = document.createElement("span");
-             newPlaceName.innerHTML = placeNameValue;
+             newPlaceName.innerHTML = data.name;
              nameCmp.appendChild(newPlaceName);
              let newWindVal = document.createElement("span");
              newWindVal.classList.add("windMarginAdjustment");
@@ -37,15 +32,13 @@
              newCloudValue.classList.add("cloudMarginAdjustment");
              let newMaxTempValue = document.createElement("span");
              newMaxTempValue.classList.add("tempMarginAdjustment");
-             newMaxTempValue.innerHTML = maxTempValue;
+             newMaxTempValue.innerHTML = data.main.temp_max;
              weatherMaxTempCmp.appendChild(newMaxTempValue);
-             newCloudValue.innerHTML = cloudValue;
+             newCloudValue.innerHTML = data.weather[0].description;
              weatherCloudyCmp.appendChild(newCloudValue);
-             temperatureCmp.innerHTML = currentTemperature;
-             newWindVal.innerHTML = windyValue;
+             temperatureCmp.innerHTML = data.main.temp;
+             newWindVal.innerHTML = data.wind.speed;
              weatherWindCmp.appendChild(newWindVal);
-
-
          })
          .catch(error => alert("Wrong City Name"))
  });
