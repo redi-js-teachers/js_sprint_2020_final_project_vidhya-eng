3dd3feb2ca20e1adf95da9e1b4bc3daf9e91f5ed
Vidhya-eng <57456491+Vidhya-eng@users.noreply.github.com>
2020-06-08 22:06:46 +0200

    Weather with api (#10)
    * Days with array and more api
    
    * generated days with array and added api
    
    * tried to create a helper function
    
    * tried to resolve conflicts
    
    * deleted fivedayforecast function and other comments
    
    Co-authored-by: Ivan Timofeev <ivan.timofeev@sixt.com>

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const date = new Date();
 document.getElementById('timeandDate').innerHTML = `${date.toDateString()}, ${date.toLocaleTimeString()}`;
-document.getElementById('timeandDate').style.color = "white";
-document.getElementById('timeandDate').style.marginTop = "200px";
-document.getElementById('timeandDate').style.marginLeft = "250px";
-document.getElementById('timeandDate').style.fontSize = "25px";
-
 const temperatureCmp = document.getElementById("weatherTemperature");
 const nameCmp = document.getElementById("name");
 const weatherMaxTempCmp = document.getElementById("weatherMaxTemp");
@@ -39,4 +34,69 @@ searchBtnCmp.addEventListener("click", function() {
             weatherWindCmp.appendChild(newWindVal);
         })
         .catch(error => alert("Wrong City Name"))
+});
+const daysList = document.getElementById("nextDaysWeather");
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+const nextDays = 4;
+
+function createDays() {
+    let today = new Date();
+    let daysSorted = [];
+    for (let i = 0; i < nextDays; i++) {
+        let newDate = new Date(today.setDate(today.getDate() + 1));
+        daysSorted.push(days[newDate.getDay()]);
+        let newLiElement = document.createElement("li");
+        newLiElement.textContent = daysSorted[i];
+        daysList.appendChild(newLiElement);
+    }
+    daysList.style.color = "white";
+    daysList.style.listStyle = "none";
+    daysList.style.padding = "15px";
+};
+createDays();
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Munich&units=metric&appid=08a09c29086a3f06cd37337b12b1711f";
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        let tempArray = [];
+        tempArray.push(data.list[1].main.temp);
+        tempArray.push(data.list[2].main.temp);
+        tempArray.push(data.list[10].main.temp);
+        tempArray.push(data.list[18].main.temp);
+
+        function displayTemperature() {
+            let listItem = document.querySelectorAll("ul > li");
+            for (let i = 0; i < tempArray.length && i < listItem.length; i++) {
+                let newTempEle = document.createElement("span");
+                newTempEle.classList.add("temperatureStyle")
+                newTempEle.innerHTML = tempArray[i];
+                listItem[i].appendChild(newTempEle);
+            }
+        }
+        displayTemperature();
+        let iconArray = [];
+        iconArray.push(data.list[0].weather[0].icon);
+        iconArray.push(data.list[2].weather[0].icon);
+        iconArray.push(data.list[10].weather[0].icon);
+        iconArray.push(data.list[18].weather[0].icon);
+    })
+
+/* tried to fetch api url from flicker for changing background images,but encountering with an error */
+searchBtnCmp.addEventListener("click", function() {
+
+    const cityUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=528fc5e2ec09109cd0696b0555e7bc81&tags=" + weatherSearchBar.value + "&format=json&nojsoncallback=1";
+    console.log(cityUrl);
+    fetch(cityUrl)
+        .then(response => response.json())
+        .then(data => {
+
+            let farmId = photos.photo[0].id;
+            let serverId = photos.photo[0].server;
+            let id = photos.photo[0].id;
+            let secretId = photos.photo[0].secret;
+            let backgroundImageUrl = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + id + "_" + secretId + ".jpg";
+            console.log(backgroundImageUrl);
+            document.getElementsByClassName("leftContainer").css('background-image', 'url(' + backgroundImageUrl + ')');
+        })
 });
